5695026f6f239dcf203263d5d9ea9c0156ee5187
Obdurat <omicron_rpl@hotmail.com>
2023-04-22 03:51:29 -0300

    feat: Added app class to bootstrap the application

---
+++ src/main/app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import {endpoints} from "./routes";
+import helmet from 'helmet'
+import cors from 'cors'
+import { ErrorHandler } from "./middleware/error.handler";
+import morgan from 'morgan';
+// Check https://github.com/davidbanham/express-async-errors for more info
+require('express-async-errors');
+
+class App {
+  private app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.middleware();
+  }
+
+  start(port: number, callback: () => void) {
+    this.app.listen(port, callback);
+  }
+
+  private middleware() {
+    // Check https://github.com/expressjs/morgan for more info
+    this.app.use(morgan('[:date] :method :url :status - :user-agent'));
+    this.app.use(express.json());
+    this.app.use(endpoints);
+    // Check https://github.com/helmetjs/helmet for more info
+    this.app.use(helmet());
+    // Check https://github.com/expressjs/cors for more info
+    this.app.use(cors({origin: '*'}));
+    this.app.use(ErrorHandler)
+    // Add more middlewares as needed here
+  }
+}
+
+export default new App;
